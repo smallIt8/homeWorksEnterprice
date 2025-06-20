e0be10177d226010353736de41444f33238a9462
smallIt8 <smallIt88@gmail.com>
2025-06-20 17:29:47 +0300

    Update files

--- task1/database-service/src/main/java/org/example/RecordingUserDatabase.java
+++ task1/database-service/src/main/java/org/example/RecordingUserDatabase.java
@@ -1,7 +1,5 @@
 package org.example;
 
-import com.sun.source.util.SourcePositions;
-
 public class RecordingUserDatabase {
     public void recording() {
         System.out.println("Пользователь успешно создан и записан в базу данных");
