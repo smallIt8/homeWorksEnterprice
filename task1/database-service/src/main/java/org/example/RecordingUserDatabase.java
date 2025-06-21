307bd321f1106a4dbc0289e68db07093db955705
Iliya Lukyanov <166504752+smallIt8@users.noreply.github.com>
2025-06-21 19:27:10 +0300

    Delete task1 directory

--- task1/database-service/src/main/java/org/example/RecordingUserDatabase.java
+++
@@ -1,7 +0,0 @@
-package org.example;
-
-public class RecordingUserDatabase {
-    public void recording() {
-        System.out.println("Пользователь успешно создан и записан в базу данных");
-    }
-}
--- task1/user-service/src/main/java/org/example/UserService.java
+++
@@ -1,9 +0,0 @@
-package org.example;
-
-public class UserService {
-
-    public void createUser() {
-        System.out.println("Создание пользователя.....");
-        new RecordingUserDatabase().recording();
-    }
-}
