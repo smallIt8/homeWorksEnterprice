6da18a69a99a86df44fb0868e95b18e21495da20
smallIt8 <smallIt88@gmail.com>
2025-09-18 19:42:17 +0300

    create validator in entity person, nit implements

---
+++ HW-003/src/main/java/org/example/validator/PersonValidator.java
@@ -0,0 +1,10 @@
+package org.example.validator;
+
+import org.example.dto.PersonDto;
+
+public class PersonValidator implements Validator<PersonDto>{
+
+	@Override
+	public void validate(PersonDto target) {
+	}
+}
