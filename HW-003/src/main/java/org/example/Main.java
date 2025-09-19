6885f76d4644b03a95a6a24300b33731a91058ef
smallIt8 <smallIt88@gmail.com>
2025-09-19 20:17:58 +0300

    create application.xml and bean Spring

--- HW-003/src/main/java/org/example/Main.java
+++ HW-003/src/main/java/org/example/Main.java
@@ -1,7 +1,10 @@
 package org.example;
 
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class Main {
 
 	public static void main(String[] args) {
+		var context = new ClassPathXmlApplicationContext("application.xml");
 	}
 }
